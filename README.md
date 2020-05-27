6dd2323c848a494d72cdaccc4c607f6b1354b09a
Ayush James <ayush.james1996@gmail.com>
2020-05-27 23:29:32 +1000

    adding comments to code and also looking into firebase for database integration

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 # Things to do.
 - Load more... infinity scroll
-- backend/database to upload content more easily. 
+- backend/database to upload content more easily - check into firebase!
+- for the url validity checker...could check to see if it is valid when the input is not active or tabbed out instead of at the submit button.
--- js/form.js
+++ js/form.js
@@ -1,7 +1,6 @@
-let mainText = document.getElementById("mainText");
-let submitBtn = document.getElementById("submitBtn");
+// Code realting to the content submission page.
 
-
+//checking to see if it is a video or an image. if image. we want to hide the video preview divs and also the video url input field.
 function isVideo() {
   if (document.getElementById("contentType").value == "image") {
     document.getElementById("videoType").style.display = "none";
@@ -12,12 +11,14 @@ function isVideo() {
   }
 }
 
+// On clicking the submit button, we grab the url that they provided and see if it is from youtube or vimeo and then use that to create a preview and convert the url to a video embedding.
 function submitClick() {
   let videoURL = document.getElementById("videoURL").value;
   let youtubeVid = videoURL.search("watch");
   let vimeoVid = videoURL.search("vimeo");
   let vidEmbedUrl = "";
 
+// the first is just a validation so that if they don't provide a clean url, they get an error message popup.
 if (document.getElementById("videoURL").value != "") {
     if (youtubeVid < 0 && vimeoVid < 0) {
         window.alert("Invalid URL! Please try again using one of the following formats: \nhttps://vimeo.com/420397570 \nhttps://www.youtube.com/watch?v=nUUPedePJ4o");
