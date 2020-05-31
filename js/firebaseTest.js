318ebcc44a47073006e6600d7130d2a557f5df18
Ayush James <ayush.james1996@gmail.com>
2020-06-01 00:29:15 +1000

    cleaned up firebase content retrieval js code for easy reading

--- js/firebaseTest.js
+++ js/firebaseTest.js
@@ -1,32 +1,48 @@
 let fireBaseRef = firebase.database().ref();
 let posts;
 
+//function to create an IMG element and post to the mediaContainer;
+function createImgElement(link) {
+  let imgTag = document.createElement("IMG");
+  imgTag.src = link;
+  document.getElementById("mediaContainer").appendChild(imgTag);
+}
+
+//function to create an IMG element and post to the mediaContainer;
+function createVidElement(link) {
+  let videoTag = document.createElement("iframe");
+  videoTag.src = link;
+  videoTag.width = "896";
+  videoTag.height="504";
+  document.getElementById("mediaContainer").appendChild(videoTag);
+}
+
+// on clicking the submit button, push the values into the firebase database.
 function submitClick() {
   let theURL = document.getElementById("theURL").value;
   let mediaType = document.getElementById("mediaType").value;
 
   fireBaseRef.push().set({
     mediaType: mediaType,
-    mediaURL: theURL
+    mediaURL: theURL,
+    totalImgs: 10
   });
 }
 
-// get the Data and from the data, loop through the objects and get the value!
+// get the object data from fireBase, loop through the objects, get the values and allocate to elements!
 function getData() {
   fireBaseRef.on('value', function(snapshot) {
     posts = snapshot.val();
 
     for (let data in posts) {
       if (posts[data].mediaType == "image") {
-        let imgTag = document.createElement("IMG");
-        imgTag.src = `${posts[data].mediaURL}`;
-        document.getElementById("mediaContainer").appendChild(imgTag);
+        let mediaURL = `${posts[data].mediaURL}`;
+        createImgElement(mediaURL);
+
       } else {
         let videoTag = document.createElement("iframe");
-        videoTag.src = `${posts[data].mediaURL}`;
-        videoTag.width = "896";
-        videoTag.height="504";
-        document.getElementById("mediaContainer").appendChild(videoTag);
+        let mediaURL = `${posts[data].mediaURL}`;
+        createVidElement(mediaURL);
       }
     }
   });
